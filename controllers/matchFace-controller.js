ed83a6b905529c56ac3a20bfe11c7e9d44b38af4
binmex <robin019@hotmail.com>
2024-07-16 19:32:07 -0500

    add crypto

--- controllers/matchFace-controller.js
+++ controllers/matchFace-controller.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const canvas = require('canvas');
 const faceapi = require('face-api.js');
 const NodeCache = require('node-cache');
+const {decrypt} = require('../utils/encryptation')
 
 // Caché para almacenar descriptores de imagen
 const descriptorCache = new NodeCache({ stdTTL: 3600 }); // Caché de 1 hora
@@ -45,13 +46,14 @@ async function getImageDescriptor(imageUrl) {
 exports.matchFace = async (req, res) => {
   try {
     const { imageUrl1, imageUrl2 } = req.body;
+
     if (!imageUrl1 || !imageUrl2) {
       return res.status(400).json({ error: "Two image URLs are required" });
     }
 
     const [descriptor1, descriptor2] = await Promise.all([
-      getImageDescriptor(imageUrl1),
-      getImageDescriptor(imageUrl2),
+      getImageDescriptor(decrypt(imageUrl1)),
+      getImageDescriptor(decrypt(imageUrl2)),
     ]);
 
     // Asegurarse de que los descriptores sean Float32Array
@@ -64,7 +66,7 @@ exports.matchFace = async (req, res) => {
     res.json({
       distance,
       similarity,
-      isSamePerson: similarity > 0.5,
+      isSamePerson: similarity > process.env.MATCH_VALUE || 0.6,
     });
   } catch (error) {
     console.error("Error processing images:", error);
